a8faf70a9c389d2d6fefbf04296898fb89e93f09
Kdx <sasiwong.kasantia@gmail.com>
2021-11-29 13:30:05 +0700

    day15 store managament

---
+++ day15/src/pages/TodoAdd.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Todo Add
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ day15/src/router.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router'
+import TodoAdd from './pages/TodoAdd.vue'
+import TodoList from './pages/TodoList.vue'
+const routes = [
+    {path: '/add', component: TodoAdd},
+    {path: '/list', component: TodoList}
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes,
+})
+
+export default router
