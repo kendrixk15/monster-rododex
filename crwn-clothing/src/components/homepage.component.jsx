eb77249c7098569b2e6dac52ee0d23d90058d7e5
Kdx <sasiwong.kasantia@gmail.com>
2022-02-07 21:42:45 +0700

    created project with cra and added file for homepage

---
+++ crwn-clothing/src/components/homepage.component.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HAT</h1>
+                    <span className="subtitile"> SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKET</h1>
+                    <span className="subtitile"> SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SNEAKERS</h1>
+                    <span className="subtitile"> SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WEMEN</h1>
+                    <span className="subtitile"> SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MEN</h1>
+                    <span className="subtitile"> SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
